c8f64a5ac4304f9689d82d823cd290b726f76979
Dan Vassallo <danielvassallo87@gmail.com>
2015-03-09 13:31:28 -0400

    changed age to datepicker

--- source/javascripts/_age-gate.js
+++ source/javascripts/_age-gate.js
@@ -4,11 +4,11 @@ function validateAge() {
 
     var age = (Date.now() - Bday) / (31557600000);
     if ( age < 13 ) {
-        alert('Sorry, you must be older to enter this sweepstakes.');
+        alert('Sorry, but you are not eligible for this sweepstakes.');
         return false
     }
     else if ( $.isNumeric(age) == false ){
-        alert('Sorry, you must enter a number.');
+        alert('Sorry, your age must be equal to a number.');
         return false
     }
     else{
