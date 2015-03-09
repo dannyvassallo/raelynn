31a3617493e7f29d15363239ad94c64e55c21b33
Dan Vassallo <danielvassallo87@gmail.com>
2015-03-09 12:55:04 -0400

    really fixed it

--- source/javascripts/_age-gate.js
+++ source/javascripts/_age-gate.js
@@ -2,9 +2,11 @@ function validateAge() {
     var age = document.forms["age-gate"]["age"].value;
     if ( age < 13 ) {
         alert('Sorry, you must be older to enter this sweepstakes.');
+        return false
     }
     else if ( $.isNumeric(age) == false ){
         alert('Sorry, you must enter a number.');
+        return false
     }
     else{
         $('.gate').hide('slow');
